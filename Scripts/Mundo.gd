f9586cc6a34c9efc21d7f50f010d016619f0fa55
Graziele Fagundes <grazifagundesm@gmail.com>
2024-04-25 23:13:10 -0300

    shaders

--- Scripts/Mundo.gd
+++ Scripts/Mundo.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Engine.max_fps = 144
@@ -11,6 +10,8 @@ func _ready():
 func _process(delta):
 	$FPSCounter.set_text("FPS %d" % Engine.get_frames_per_second())
 
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
