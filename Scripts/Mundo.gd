0850309d6fb780f9bb350c4ef37ec47b68dc9397
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-04-28 20:20:11 -0300

    mobile prototipo

--- Scripts/Mundo.gd
+++ Scripts/Mundo.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var mobile = $"../../Mobile"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +12,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$FPSCounter.set_text("FPS: %d" % Engine.get_frames_per_second())
-	pass
+	if Input.is_action_just_pressed("Mobile"):
+		if mobile.visible:
+			mobile.hide()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			mobile.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 func _input(event):
