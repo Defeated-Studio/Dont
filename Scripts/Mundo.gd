27ec67df554aa9ee6046999acef0146a12c70380
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-05-02 20:37:57 -0300

    mobile menu and both chats

--- Scripts/Mundo.gd
+++ Scripts/Mundo.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
-@onready var mobile = $"../../Mobile"
 
+@onready var messages_app = $"../../MessagesApp"
+@onready var mom_chat = $"../../MomChat"
+@onready var bob_chat = $"../../BobChat"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +15,17 @@ func _ready():
 func _process(delta):
 	$FPSCounter.set_text("FPS: %d" % Engine.get_frames_per_second())
 	if Input.is_action_just_pressed("Mobile"):
-		if mobile.visible:
-			mobile.hide()
+		if messages_app.visible:
+			messages_app.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
-			mobile.show()
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			if mom_chat.visible or bob_chat.visible:
+				mom_chat.hide()
+				bob_chat.hide()
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			else:
+				messages_app.show()
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 func _input(event):
