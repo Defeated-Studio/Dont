79dbe5b0cb796a28c5fd0ec08a0d6544ada945f3
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-05-07 20:22:19 -0300

    Tv glows light
    ativei ssil no world envoriment, abaixou um pouco fps, mas acho q ta de boa ainda

--- Scripts/EatTask.gd
+++ Scripts/EatTask.gd
@@ -43,6 +43,9 @@ func _ready():
 func _set_viewport_mat(_display_mesh : MeshInstance3D, _sub_viewport : SubViewport, _surface_id : int = 0):
 	var _mat : StandardMaterial3D = StandardMaterial3D.new()
 	_mat.albedo_texture = _sub_viewport.get_texture()
+	_mat.emission_enabled = true
+	_mat.emission_texture = _sub_viewport.get_texture()
+	_mat.emission_energy_multiplier = 10
 	_display_mesh.set_surface_override_material(_surface_id, _mat)
 	
 	
