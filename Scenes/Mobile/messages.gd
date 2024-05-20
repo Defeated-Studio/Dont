ff1165905741ce8798ec75b9e881b182f62a5679
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-05-20 13:50:46 -0300

    fix messages in night2

--- Scenes/Mobile/messages.gd
+++ Scenes/Mobile/messages.gd
@@ -4,8 +4,47 @@ extends Control
 @onready var bob = $Bob
 @onready var messages = $Messages
 
+@onready var second_date = $"Bob/ScrollContainer/VBoxContainer/SecondDate"
+@onready var space_25 = $"Bob/ScrollContainer/VBoxContainer/Space25"
+@onready var message_23 = $"Bob/ScrollContainer/VBoxContainer/Message23"
+@onready var space_23 = $"Bob/ScrollContainer/VBoxContainer/Space23"
+@onready var space_24 = $"Bob/ScrollContainer/VBoxContainer/Space24"
+@onready var message_24 = $"Bob/ScrollContainer/VBoxContainer/Message24"
+@onready var space_28 = $"Bob/ScrollContainer/VBoxContainer/Space28"
+@onready var no_signal = $"Bob/ScrollContainer/VBoxContainer/Message23/noSignal"
+@onready var no_signal_2 = $"Bob/ScrollContainer/VBoxContainer/noSignal2"
+@onready var space_27 = $"Bob/ScrollContainer/VBoxContainer/Space27"
+
+@onready var scrollbar = $"Bob/ScrollContainer".get_v_scroll_bar()
+@onready var scrollbarValue = $"Bob/ScrollContainer"
+var max_scroll_length = 0 
+var finishedTexting = false
+
 var inBobWindow = false
 var backButtonSignal = false
+
+func _process(delta):
+	if max_scroll_length != scrollbar.max_value: 
+		max_scroll_length = scrollbar.max_value
+		scrollbarValue.scroll_vertical = max_scroll_length
+
+func sendBobMessages():
+	await get_tree().create_timer(1.2).timeout
+	second_date.show()
+	space_25.show()
+	message_23.show()
+	space_23.show()
+	await get_tree().create_timer(1.2).timeout
+	no_signal.show()
+	space_24.show()
+	await get_tree().create_timer(2).timeout
+	message_24.show()
+	space_28.show()
+	await get_tree().create_timer(1.2).timeout
+	no_signal_2.show()
+	space_27.show()
+	finishedTexting = true
+
 
 func showMobile():
 	messages.show()
