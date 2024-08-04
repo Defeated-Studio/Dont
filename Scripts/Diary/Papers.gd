e6e358f191e0b6f7e4fa6afae97b0ae168ca206f
Otávio Rocha <otaviosrocha30@gmail.com>
2024-08-04 18:55:41 -0300

    add: paper numbers on diary

--- Scripts/Diary/Papers.gd
+++ Scripts/Diary/Papers.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var left_paper_text = $left_paper/left_paper_text
 @onready var right_paper_text = $right_paper/right_paper_text
 @onready var right_paper = $right_paper
+@onready var num_left = $num_left
+@onready var num_right = $num_right
 
 var paper = 0
 var paper_texts = ["Não sei quem vai encontrar isso, mas preciso escrever. Talvez assim as coisas fiquem mais claras pra mim também. Me mudei pra essa casa há algum tempo.
@@ -36,6 +38,8 @@ func set_visibility(is_visible: bool):
 func change_papers(to_paper, papers_taken):
 	paper = to_paper
 	if paper == 2:
+		num_left.text = "5"
+		num_right.text = " "
 		right_paper.visible = false;
 		if papers_taken[paper*2]:
 			left_paper.visible = true;
@@ -43,6 +47,9 @@ func change_papers(to_paper, papers_taken):
 		else:
 			left_paper.visible = false;
 		return;
+	
+	num_left.text = str((paper*2)+1)
+	num_right.text = str((paper*2)+2)
 	
 	if papers_taken[paper*2]:
 		left_paper.visible = true;
@@ -57,6 +64,8 @@ func change_papers(to_paper, papers_taken):
 		right_paper.visible = false;
 
 func start_papers(papers_taken):
+	num_left.text = str(1)
+	num_right.text = str(2)
 	if papers_taken[0] == 1:
 		left_paper.visible = true;
 		left_paper_text.text = paper_texts[0];
